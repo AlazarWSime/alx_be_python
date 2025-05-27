bef126c0e4de7c4968e036b3f20b55a19947b1f0
Alazar W. Sime <alazarwondwessen47@gamil.com>
2025-05-27 05:01:35 -0700

    modified fiance_calculator

--- python_introduction/finance_calculator.py
+++ python_introduction/finance_calculator.py
@@ -2,4 +2,4 @@ monthly_income = float(input("Enter your monthly income: " ))
 total_monthly_expenses = float(input("Enter your total monthly expenses: "))
 monthly_savings = total_monthly_expenses - monthly_income
 projected_savings = monthly_savings * 12 + (monthly_savings * 12 * 0.05)
-print(f"Projected davings after one year, with interest, is:{projected_savings} ")
+print(f"Projected davings after one year, with interest, is: ${projected_savings} ")
