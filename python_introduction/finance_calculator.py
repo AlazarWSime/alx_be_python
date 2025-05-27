04b97f01a452110082f514a26b697e2bd8592699
Alazar W. Sime <alazarwondwessen47@gamil.com>
2025-05-27 10:45:53 -0700

    fianacail_calculator modified code

--- python_introduction/finance_calculator.py
+++ python_introduction/finance_calculator.py
@@ -4,5 +4,5 @@ total_monthly_expenses = input("Enter your total monthly expenses: ")
 
 monthly_savings = float(monthly_income) - float(total_monthly_expenses)
 projected_savings = monthly_savings * 12 + (monthly_savings * 12 * 0.05)
-
+print(f"your montly saving is: ${monthly_savings}")
 print(f"Projected davings after one year, with interest, is: ${projected_savings} ")
