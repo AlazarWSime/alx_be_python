972572f0fc908dac714fcbac8c4fd876ee8d4358
Alazar W. Sime <alazarwondwessen47@gamil.com>
2025-06-15 16:28:50 +0300

    i have modifed the function name and parametr again

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -1,18 +1,16 @@
-def perform_operation(num1: float, num2: float, operation: str):
-     if operation == "add":
-          return num1 + num2
-     elif operation == "subtract":
-          return num1 - num2
-     elif operation == "multiply":
-          return num1 * num2
-     elif operation == "divide":
-          if num2 != 0:
-               return num1/num2
-          else:
-               print("Erro: Division by zero!")
-     else:
-          print("Invalid operation! Please choose from add, subtract, multiply, or divide.")
-     
-
-
-         
+def perform_operation(num1, num2, operation):
+    if operation == "add":
+        return num1 + num2
+    elif operation == "subtract":
+        return num1 - num2
+    elif operation == "multiply":
+        return num1 * num2
+    elif operation == "divide":
+        if num2 != 0:
+            return num1 / num2
+        else:
+            print("Error: Division by zero!")
+            return None
+    else:
+        print("Invalid operation! Please choose from add, subtract, multiply, or divide.")
+        return None
