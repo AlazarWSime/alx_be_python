f45242188e8e1d5ea262e448bc7edd85f69c781a
Alazar W. Sime <alazarwondwessen47@gamil.com>
2025-06-15 21:21:29 +0300

    modified version of the code for the second time

--- fns_and_dsa/shopping_list_manager.py
+++ fns_and_dsa/shopping_list_manager.py
@@ -1,5 +1,4 @@
 def display_menu():
-
     print("Shopping List Manager")
     print("1. Add Item")
     print("2. Remove Item")
@@ -8,26 +7,24 @@ def display_menu():
 
 def main():
     shopping_list = []
-                      
     while True:
         display_menu()
-        choice = int(input("Enter your number(1 - 4): "))
+        choice = input("Enter your choice: ")
 
-        if choice == 1:
-           item = input("what item do you want to add: ")
-           shopping_list.append(item) 
-           pass
-        elif choice == 2:
-           item = input("what item do you want to remove: ")
-           if item in shopping_list:
-                  shopping_list.remove(item)
-           else:
-               print("The item you want to remove doesn't exist in your shopping list!")
-           pass
-        elif choice == 3:
-            print(f"Your shoping List is: {shopping_list}")
-            pass
-        elif choice == 4:
+        if choice == '1':
+            item = input("Enter the item to add: ")
+            shopping_list.append(item)
+        elif choice == '2':
+            item = input("Enter the item to remove: ")
+            if item in shopping_list:
+                shopping_list.remove(item)
+            else:
+                print("The item is not in the list.")
+        elif choice == '3':
+            print("Your shopping list:")
+            for item in shopping_list:
+                print(f"- {item}")
+        elif choice == '4':
             print("Goodbye!")
             break
         else:
