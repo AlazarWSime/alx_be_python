1b9a3de741e161ad0ab2ee13432be2b90255c18f
Alazar W. Sime <alazarwondwessen47@gamil.com>
2025-06-15 16:04:49 +0300

    my fires commite of the directroy fns_and_dsa

---
+++ fns_and_dsa/arithmetic_operations.py
@@ -0,0 +1,18 @@
+def perform_operation(num1: float, num2: float, operation: str):
+     if operation == "add":
+          return num1 + num2
+     elif operation == "subtract":
+          return num1 - num2
+     elif operation == "multiply":
+          return num1 * num2
+     elif operation == "divide":
+          if num2 != 0:
+               return num1/num2
+          else:
+               print("Erro: Division by zero!")
+     else:
+          print("Invalid operation! Please choose from add, subtract, multiply, or divide.")
+          return None
+
+
+         
